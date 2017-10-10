f214ffc3b71d3ffbe8004dee70fd7369fb5e334d
Sewmi <seud721@gmail.com>
2017-10-10 06:06:40 +0530

    Function Added

--- StudentManagementSystem_CSSE/src/Controller/Controller_Student.java
+++ StudentManagementSystem_CSSE/src/Controller/Controller_Student.java
@@ -6,18 +6,34 @@
 package Controller;
 import DBConnection.DBConnect;
 import java.sql.Connection;
+import Model.Model_Student;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 /**
  *
  * @author sewmi
  */
 public class Controller_Student {
-     static Connection conn=null;
+    static Connection conn=null;
      
-     public Controller_Student() {
-        conn=DBConnect.connect();   
+//     public Controller_Student() {
+//        conn=DBConnect.connect();   
+//    }
+    public static void AddStudent(Model_Student Student) throws SQLException{
+        conn=DBConnect.connect();     
+        String query="INSERT INTO Students(fName,lName, nic, phone,course, department) VALUES('"
+                + Student.getFname() +"','"+ Student.getLname() +"','"+ Student.getNic() +"',"
+                + Student.getPhone() +",'"+ Student.getCourse() +"','"+ Student.getDepartment() +"');" ;       
+        Statement stmt = conn.createStatement();
+        stmt.executeQuery(query);   
     }
-     
+    
+    
+    
+    
+
      
      
      
--- StudentManagementSystem_CSSE/src/Model/Model_Student.java
+++ StudentManagementSystem_CSSE/src/Model/Model_Student.java
@@ -13,13 +13,50 @@ public class Model_Student {
     public String FName, LName , Nic, Course, Department;
     public int Phone;
     
-    public Model_Student(String fName,String lName ,String nic,String course,String department, int phone)
-    {
+    public Model_Student(String fName,String lName ,String nic,String course,String department, int phone) {
         FName = fName;
         LName = lName;
         Nic = nic;
         Course = course;
         Department = department;
+        Phone = phone;
     };
+      
+    public void StoreFname(String fName){
+        FName=fName;  
+    }
+    public String getFname(){
+        return FName;
+    }
+    public void StoreLname(String lName){
+        LName=lName;  
+    }
+    public String getLname(){
+        return LName;
+    }
+    public void StoreNic(String nic){
+        Nic=nic;  
+    }
+    public String getNic(){
+        return Nic;
+    }
+    public void StoreCourse(String course){
+        Course=course;  
+    }
+    public String getCourse(){
+        return Course;
+    }
+    public void StoreDepartment(String department){
+        Department=department;  
+    }
+    public String getDepartment(){
+        return Department;
+    }
+    public void StorePhone(int phone){
+        Phone= phone;  
+    }
+    public int getPhone(){
+        return Phone;
+    }
     
 }
