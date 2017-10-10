48f3780b9a7f9c7d87606029819eaaf792cbc700
Sewmi <seud721@gmail.com>
2017-10-10 15:49:04 +0530

    updated code

--- StudentManagementSystem_CSSE/src/Controller/Controller_Student.java
+++ StudentManagementSystem_CSSE/src/Controller/Controller_Student.java
@@ -7,35 +7,56 @@ package Controller;
 import DBConnection.DBConnect;
 import java.sql.Connection;
 import Model.Model_Student;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 
-/**
- *
+/*
  * @author sewmi
  */
+
 public class Controller_Student {
-    static Connection conn=null;
-     
-//     public Controller_Student() {
-//        conn=DBConnect.connect();   
-//    }
+    static Connection dbConn=null;
+
     public static void AddStudent(Model_Student Student) throws SQLException{
-        conn=DBConnect.connect();     
+        dbConn=DBConnect.connect();     
         String query="INSERT INTO Students(fName,lName, nic, phone,course, department) VALUES('"
                 + Student.getFname() +"','"+ Student.getLname() +"','"+ Student.getNic() +"',"
                 + Student.getPhone() +",'"+ Student.getCourse() +"','"+ Student.getDepartment() +"');" ;       
-        Statement stmt = conn.createStatement();
+        Statement stmt = dbConn.createStatement();
         stmt.executeQuery(query);   
+        dbConn.close();
+    }
+    public static void UpdateStudent(Model_Student Student) throws SQLException{
+        dbConn=DBConnect.connect();     
+        String query="UPDATE Students SET fName = '',lName='', nic='', phone='',course='', department='';";
+               // + Student.getFname() +"','"+ Student.getLname() + "','"+ Student.getNic() +"',"
+               // + Student.getPhone() +",'"+ Student.getCourse() + "','"+ Student.getDepartment() +"');" ;       
+        Statement stmt = dbConn.createStatement();
+        stmt.executeQuery(query);   
+        dbConn.close();
+    }
+    public static void DeleteStudent(String stuId) throws SQLException{
+        dbConn=DBConnect.connect();     
+        String query="DELETE FROM Students WHERE id="+ stuId +";";
+        Statement stmt = dbConn.createStatement();
+        stmt.executeQuery(query);   
+        dbConn.close();
     }
     
+    static PreparedStatement pst=null;
+    static ResultSet rs=null;
+    public static ResultSet loadFacultytable() throws SQLException{
+        dbConn=DBConnect.connect();
     
-    
-    
-
-     
-     
-     
+        String query="SELECT * FROM Faculty";
+        pst=dbConn.prepareStatement(query);
+        rs=pst.executeQuery();
+      
+        return rs;
+    }
+  
      
 }
