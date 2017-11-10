38c56e0e0c81895f9b28910b8def15374fc6b6c3
Rishni <Rishni@DESKTOP-7681NTT>
2017-11-10 17:33:28 +0530

    commit

--- StudentManagementSystem_CSSE/test/Controller/StaffControllerNGTest.java
+++ StudentManagementSystem_CSSE/test/Controller/StaffControllerNGTest.java
@@ -1,29 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *This is the test case class to test
+ *AddStaff method in StaffController class
+ *which insert a staff member to staff table.
  */
 package Controller;
 
-import static Controller.StaffController.conn;
 import DBConnection.DBConnect;
-import Model.FacultyModel;
 import Model.StaffModel;
-import java.sql.ResultSet;
-import org.testng.Assert;
-import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.Assert;
+import static Controller.StaffController.conn;
 
 /**
  *
  * @author Rishni
  */
 public class StaffControllerNGTest {
-    
+    //values assigned to constants to be added to staff table
     public static final String FNAME="Manel";
     public static final String LNAME="Fonseka";
     public static final int FACULTY=9;
@@ -35,10 +32,12 @@ public class StaffControllerNGTest {
     StaffModel staffmodel;
     
     public StaffControllerNGTest() {
+        //connecting to the database
         conn=DBConnect.connect();
+        //creating a new object from StaffController
         staffcontroller=new StaffController();
-        staffmodel=new StaffModel(FNAME,LNAME,FACULTY,POSITION,NIC);
-        
+        //creating a new object from StaffModel
+        staffmodel=new StaffModel(FNAME,LNAME,FACULTY,POSITION,NIC);        
     }
 
     @BeforeClass
@@ -59,14 +58,11 @@ public class StaffControllerNGTest {
 
     /**
      * Test of AddStaff method, of class StaffController.
+     * @throws java.lang.Exception
      */
     @Test
     public void testAddStaff() throws Exception {
-
           Assert.assertTrue(staffcontroller.AddStaff(staffmodel));
-         
     }
 
-
-    
 }
